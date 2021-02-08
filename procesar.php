66beb4590d2b0f77c67c8f3f9cdc095103d8ecfc
Esteban <estebanrosmartinez@gmail.com>
2021-02-08 09:47:38 +0100

    PRIMER PROYECTO

---
+++ procesar.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+<title></title>
+<meta charset="UTF-8">
+</head>
+<body>
+<form name="formulario" action="procesar.php" method="POST">
+	Introduce tu nombre: <input type="text" name="nombre">
+	Introduce la edad: <input type="text" name="edad">
+	<input type="submit" value="Enviar">
+</form>
+</body>
+</html>
+<?php
+if($_POST) {
+   foreach($_POST as $clave=>$valor) {
+           echo "El valor de $clave es: $valor"."<br />";
+   }
+}
+?>
